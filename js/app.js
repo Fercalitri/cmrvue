db1e8d7b224376f23c79718f3ebe62b1d0b529d5
fcalitri <fcalitri@psiware.com.ar>
2023-10-10 15:16:17 -0300

    Se añadio funcion de checkout y se modifico el template del cart para agregar o quitar lo s productos desde ahi

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ new Vue({
         isShowingCart: false,
         cart: {
             items: []
-        }, 
+        },
         products: [
             {
                 id: 1,
@@ -49,26 +49,11 @@ new Vue({
                 inStock: 81
             }
         ]
-    }, 
-
-    computed:{
-        cartTotal: function() {
-            var total = 0;
-
-            this.cart.items.forEach(function(item){
-                total += item.quantity * item.product.price;
-            });
-            return total;
-        },
-
-        taxAmount: function(){
-            return ((this.cartTotal * 10) / 100);
-        }
     },
-
-    methods:{
-        addProductToCart: function(product){
+    methods: {
+        addProductToCart: function(product) {
             var cartItem = this.getCartItem(product);
+        
             if (cartItem != null) {
                 cartItem.quantity++;
             } else {
@@ -77,27 +62,69 @@ new Vue({
                     quantity: 1
                 });
             }
-            
+        
             product.inStock--;
         },
-
         getCartItem: function(product) {
-            for (var i = 0; i < this.cart.items.length; i++){
-                if (this.cart.items[i].product,id === product.id) {
-                    return this.cart.items [i];
+            for (var i = 0; i < this.cart.items.length; i++) {
+                if (this.cart.items[i].product.id === product.id) {
+                    return this.cart.items[i];
                 }
             }
+        
             return null;
+        },
+        increaseQuantity: function(cartItem) {
+            cartItem.product.inStock--;
+            cartItem.quantity++;
+        },
+        decreaseQuantity: function(cartItem) {
+            cartItem.quantity--;
+            cartItem.product.inStock++;
+        
+            if (cartItem.quantity == 0) {
+                this.removeItemFromCart(cartItem);
+            }
+        },
+        removeItemFromCart: function(cartItem) {
+            var index = this.cart.items.indexOf(cartItem);
+        
+            if (index !== -1) {
+                this.cart.items.splice(index, 1);
+            }
+        },
+        checkout: function() {
+            if (confirm('Are you sure that you want to purchase these products?')) {
+                this.cart.items.forEach(function(item) {
+                    item.product.inStock += item.quantity;
+                });
+            
+                this.cart.items = [];
+            }
         }
     },
-
+    computed: {
+        cartTotal: function() {
+            var total = 0;
+        
+            this.cart.items.forEach(function(item) {
+                total += item.quantity * item.product.price;
+            });
+        
+            return total;
+        },
+        taxAmount: function() {
+            return ((this.cartTotal * 10) / 100);
+        }
+    },
     filters: {
         currency: function(value) {
-            var formatter = Intl.NumberFormat('en-US', {
+            var formatter = new Intl.NumberFormat('en-US', {
                 style: 'currency',
                 currency: 'USD',
                 minimumFractionDigits: 0
             });
+            
             return formatter.format(value);
         }
     }
